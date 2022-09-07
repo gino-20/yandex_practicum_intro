513d1e2b886cfd0d4e2cfa279680081c9275ab2d
alex <alexey.v.gaydukov@yandex.ru>
2022-09-07 20:55:59 +0300

    Внесены стилистические исправления, локальные тесты пройдены

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 """Генератор приветствий."""
-import pprint
+import sys
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Args:
+        name(str): Имя пользователя
+
+    Returns:
+        str: Текст приветствия
+    """
+    return 'Привет, {0}'.format(name.title())
+
+
+if __name__ == '__main__':
+    greeting(str(sys.argv[1]))
